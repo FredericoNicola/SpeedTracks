8d3da7c99388d9b27a8f5e09e19f98ab2b79c88e
Frederico Nicola <fredericonicola@gmail.com>
2022-06-16 17:42:39 +0100

    remove Login

--- app/src/main/java/com/example/speedtracks/ui/login/ui/LoginViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.speedtracks.ui.login.ui
-
-import androidx.lifecycle.ViewModel
-
-class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/example/speedtracks/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/speedtracks/ui/notifications/NotificationsFragment.kt
@@ -1,13 +1,16 @@
 package com.example.speedtracks.ui.notifications
 
 import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.speedtracks.MainActivity
 import com.example.speedtracks.ShoeAdapter
@@ -26,7 +29,7 @@ class NotificationsFragment : Fragment() {
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         val notificationsViewModel =
                 ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
@@ -57,7 +60,9 @@ class NotificationsFragment : Fragment() {
 
         }
 
-        binding.listviewProfile.adapter = ShoeAdapter(context = ,shoeArrayList)
+        val i = context
+
+      //  binding.listviewProfile.adapter = ShoeAdapter(,shoeArrayList)
 
 
         return root
