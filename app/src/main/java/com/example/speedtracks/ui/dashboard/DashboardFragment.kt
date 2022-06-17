6cab91cc63941923a43ff017174a58e0b02ce9b8
Frederico Nicola <fredericonicola@gmail.com>
2022-06-17 14:45:19 +0100

    Spinner set data

--- app/src/main/java/com/example/speedtracks/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/speedtracks/ui/dashboard/DashboardFragment.kt
@@ -1,13 +1,19 @@
 package com.example.speedtracks.ui.dashboard
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.example.speedtracks.R
 import com.example.speedtracks.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
@@ -30,11 +36,49 @@ class DashboardFragment : Fragment() {
         val root: View = binding.root
 
         binding.nextBtn.setOnClickListener{
-            binding.brandentry.isVisible = false
             binding.brandtext.isVisible = false
             binding.nextBtn.isVisible = false
             binding.spinnerModels.isVisible = true
             binding.Addbtn.isVisible = true
+        }
+
+        val spinner: Spinner = binding.brandtext
+// Create an ArrayAdapter using the string array and a default spinner layout
+        context?.let {
+            ArrayAdapter.createFromResource(
+                it,
+                R.array.shoes_array,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                // Apply the adapter to the spinner
+                spinner.adapter = adapter
+            }
+        }
+
+        val spinner2: Spinner = binding.spinnerModels
+// Create an ArrayAdapter using the string array and a default spinner layout
+        context?.let {
+            ArrayAdapter.createFromResource(
+                it,
+                R.array.model_array,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                // Apply the adapter to the spinner
+                spinner2.adapter = adapter
+            }
+        }
+
+        binding.Addbtn.setOnClickListener {
+            Toast.makeText(
+                context,
+                "Shoe Added",
+                Toast.LENGTH_LONG
+            ).show()
+
         }
         return root
     }
